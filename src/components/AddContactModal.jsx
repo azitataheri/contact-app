2e1a8191dab2c05527932e400ff75f9856267c0c
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-02-09 17:26:58 +0330

    add contacts to table

--- src/components/AddContactModal.jsx
+++ src/components/AddContactModal.jsx
@@ -1,12 +1,36 @@
 import styles from "../components/AddContactModal.module.css";
 import close from "../assets/img/close.png";
 
-function AddContactModal({ showModal, setShowModal }) {
+function AddContactModal({
+  showModal,
+  setShowModal,
+  contact,
+  setContact,
+  addNewContact,
+}) {
   const closeModalHandler = () => {
     console.log("kfkkdfkdf");
 
     setShowModal((showModal) => !showModal);
   };
+
+  const addContactHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+
+  const submitHandler = () => {
+    addNewContact(contact);
+    setShowModal(false);
+    setContact({
+      name: "",
+      lastName: "",
+      email: "",
+    });
+  };
+
   return (
     <div className={styles.modal}>
       <div className={styles.content}>
@@ -19,16 +43,38 @@ function AddContactModal({ showModal, setShowModal }) {
         </div>
         <div className={styles.form}>
           <div className={styles.formgroup}>
-            <label htmlFor="">نام و نام خانوادگی</label>
-            <input type="text" name="name" />
+            <label htmlFor="name">نام:</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={contact.name}
+              onChange={addContactHandler}
+            />
           </div>
 
           <div className={styles.formgroup}>
-            <label htmlFor="">ایمیل:</label>
-            <input type="email" name="email" />
+            <label htmlFor="lname">نام خانوادگی:</label>
+            <input
+              id="lname"
+              type="text"
+              name="lastName"
+              value={contact.lastName}
+              onChange={addContactHandler}
+            />
+          </div>
+          <div className={styles.formgroup}>
+            <label htmlFor="email">ایمیل:</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={contact.email}
+              onChange={addContactHandler}
+            />
           </div>
           <div>
-            <button>اعمال تغییرات</button>
+            <button onClick={submitHandler}>اعمال تغییرات</button>
           </div>
         </div>
       </div>
