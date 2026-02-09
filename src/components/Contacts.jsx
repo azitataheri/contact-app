b253d714183cee572673a995f0e56099c1322ea9
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-02-09 12:20:38 +0330

    craate contacts file and styles them

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import styles from "../css/Contacts.module.css";
+
 function Contacts() {
+  const [contact, setContact] = useState({
+    name: "",
+    astName: "",
+    email: "",
+  });
+
+  const [contacts, setContacts] = useState([])
+
+
+  // add contact
+  const addContactHandler = () => {
+    console.log('add');
+        
+  }
   return (
-    <div>Contacts</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.search}>
+          <button>search</button>
+        </div>
+        <div>
+          <button onClick={addContactHandler}>+</button>
+        </div>
+      </div>
+      <table className={styles.table}>
+        <tr>
+          <th>نام و نام خانوادگی</th>
+          <th>ایمیل</th>
+          <th>عملیات</th>
+        </tr>
+        <tr>
+          <td>علی رضایی</td>
+          <td>ali@gmail.com</td>
+          <td>lldlldf</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-export default Contacts
+export default Contacts;
